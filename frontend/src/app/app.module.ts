c67d0931efd25ae41b1ec98505f14657b08b7666
BiancaOprisiu <oprisiu.bianca@yahoo.com>
2024-03-15 20:01:32 +0200

    Added seats map

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FloorMapComponent } from './floor-map/floor-map.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatTooltip, MatTooltipModule} from "@angular/material/tooltip";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FloorMapComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/floor-map/floor-map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-floor-map',
+  templateUrl: './floor-map.component.html',
+  styleUrls: ['./floor-map.component.scss']
+})
+export class FloorMapComponent {
+
+}
