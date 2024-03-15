6f572d4bdf6ae1f725c3f5857562e81b291036f9
Darius <darius.jurjiu@yahoo.com>
2024-03-15 17:26:50 +0200

    Changed database schema.

--- backend/models/room.py
+++ backend/models/room.py
@@ -1,5 +1,12 @@
 from database import db
+from models.bookable import Bookable
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import ForeignKey
 
-class Room(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    bookable_id = db.Column(db.Integer, db.ForeignKey('bookable.id'), nullable=False)
+
+class Room(Bookable):
+    id = mapped_column(ForeignKey("bookable.id"), primary_key=True)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "desk",
+    }
--- backend/models/user.py
+++ backend/models/user.py
@@ -7,6 +7,12 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(100), nullable=False)
-    role = db.Column(Enum('admin', 'employee', name='user_role_enum'), nullable=False)
+    role = db.Column(Enum("admin", "employee", name="user_role_enum"), nullable=False)
 
-    bookings = db.relationship('Booking', backref='user', lazy=True)
+    bookings = db.relationship("Booking", back_populates="user", lazy=True)
+
+    def __init__(self, email, password, username, role):
+        self.email = email
+        self.password = password
+        self.username = username
+        self.role = role
