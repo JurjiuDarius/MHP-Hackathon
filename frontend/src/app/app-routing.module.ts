8a44b04b508401c2ee7050f44c855248544ff681
Darius <darius.jurjiu@yahoo.com>
2024-03-15 19:29:38 +0200

    Added my-bookings component.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { MyBookingsComponent } from './my-bookings/my-bookings.component';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'login',
     component: LoginComponent,
   },
+  { path: 'my-bookings', component: MyBookingsComponent },
 ];
 
 @NgModule({
