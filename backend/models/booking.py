849fb2eacc0ab712361ccfefba863287dcd6b2ff
kgypeter <kgyulapeter10@gmail.com>
2024-03-15 16:50:56 +0200

    Intiial db model

---
+++ backend/models/booking.py
@@ -0,0 +1,10 @@
+from database import db
+
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    start = db.Column(db.Time, nullable=False)
+    end = db.Column(db.Time, nullable=False)
--- backend/models/user.py
+++ backend/models/user.py
@@ -1,6 +1,12 @@
+from sqlalchemy import Enum
 from database import db
-from sqlalchemy.orm import mapped_column
+
 
 class User(db.Model):
-    id = mapped_column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), nullable=False)
+    role = db.Column(Enum('admin', 'employee', name='user_role_enum'), nullable=False)
 
+    bookings = db.relationship('Booking', backref='user', lazy=True)
