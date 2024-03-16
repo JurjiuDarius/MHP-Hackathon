fe41d6b79d517dcf469052e8a5b3ca687b6acfb8
BiancaOprisiu <oprisiu.bianca@yahoo.com>
2024-03-16 16:48:40 +0200

    fixed time and date

--- frontend/src/app/book-desk-dialog/book-desk-dialog.component.ts
+++ frontend/src/app/book-desk-dialog/book-desk-dialog.component.ts
@@ -1,19 +1,21 @@
-import {Component, Inject} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Inject, ViewChild} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogActions, MatDialogContent, MatDialogRef} from "@angular/material/dialog";
 import {Booking} from "../models/booking";
 import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatOption, MatSelect} from "@angular/material/select";
 import {MatCheckbox} from "@angular/material/checkbox";
-import {NgForOf, NgIf} from "@angular/common";
+import {DatePipe, NgForOf, NgIf} from "@angular/common";
 import {MatButton} from "@angular/material/button";
 import {User} from "../models/user";
 import {UserService} from "../service/user.service";
 import {BookingService} from "../service/booking.service";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-book-desk-dialog',
   standalone: true,
+  providers: [DatePipe],
   imports: [
     MatDialogActions,
     MatDialogContent,
@@ -27,20 +29,22 @@ import {BookingService} from "../service/booking.service";
     NgForOf,
     MatCardHeader,
     MatButton,
-    NgIf
+    NgIf,
+    FormsModule
   ],
   templateUrl: './book-desk-dialog.component.html',
   styleUrl: './book-desk-dialog.component.scss'
 })
-export class BookDeskDialogComponent {
+export class BookDeskDialogComponent{
 
-  people: string[] = ['Item 1', 'Item 2', 'Item 3'];
   public users: User[] = []
   public bookings: Booking[] = []
   selectedPeople: string[] = [];
+  startTime: string='';
+  endTime: string='';
 
   constructor(public dialogRef: MatDialogRef<BookDeskDialogComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: Booking, private userService: UserService, private bookingService: BookingService){
+              @Inject(MAT_DIALOG_DATA) public data: Booking, private datePipe: DatePipe, private userService: UserService, private bookingService: BookingService){
     this.userService.getUsers().subscribe({
       next: (response) => {
         this.users = response;
@@ -74,10 +78,22 @@ export class BookDeskDialogComponent {
     this.dialogRef.close();
   }
 
+  getFormattedDate(currentDate:Date): string {
+    if (currentDate) {
+      return this.datePipe.transform(currentDate, 'MM/dd/yyyy') || '';
+    }
+    return '';
+  }
+
   onConfirm(): void {
-    // Perform confirmation logic here
     console.log(this.selectedPeople)
     console.log(this.data)
+    if(this.data.date.length==0){
+      const currentDate=new Date();
+      this.data.date = this.getFormattedDate(currentDate);
+    }
+    this.data.start=this.startTime;
+    this.data.end=this.endTime;
     this.bookingService.createBooking(this.data);
     this.dialogRef.close();
   }
