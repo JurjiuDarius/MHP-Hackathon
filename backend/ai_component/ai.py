530abea7a1e538c59ffd822e61cddeac71a97f52
MihAMi02 <mihai.mihalache@stud.ubbcluj.ro>
2024-03-16 16:01:01 +0200

    saved ai models and made ai_interactive_logic.py to access said models

--- backend/ai_component/ai.py
+++ backend/ai_component/ai.py
@@ -1,3 +1,6 @@
+import pickle
+from datetime import datetime
+
 from numpy import loadtxt
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
@@ -12,124 +15,261 @@ from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+import numpy as np
+# import tensorflow as tf
+# #
+def get_day_of_week_numeric(date_str):
+    # Convert the string to a datetime object
+    date_object = datetime.strptime(date_str, '%d/%m/%Y')
+    # Get the day of the week (0 = Monday, 1 = Tuesday, ..., 6 = Sunday)
+    day_of_week_numeric = date_object.weekday()
+    return day_of_week_numeric
+# #
+# def one_hot_encode(day_of_week_numeric):
+#     # Define the number of classes (days of the week)
+#     num_classes = 135
+#     # Create one-hot encoding
+#     one_hot = tf.one_hot(day_of_week_numeric, num_classes)
+#     return one_hot
+#
+# def one_hot_encode_small(day_of_week_numeric):
+#     # Define the number of classes (days of the week)
+#     num_classes = 6
+#     # Create one-hot encoding
+#     one_hot = tf.one_hot(day_of_week_numeric, num_classes)
+#     return one_hot
+#
+# def get_conference_room_number(conference_room):
+#     if conference_room == "Quick 8":
+#         return 0
+#     if conference_room == "Pit-Lane":
+#         return 1
+#     if conference_room == "Dry-lane":
+#         return 2
+#     if conference_room == "Joker Lap":
+#         return 3
+#     if conference_room == "Pole Position":
+#         return 4
+#     if conference_room == "Cockpit":
+#         return 5
+# def one_hot_encode_conference(conference_room_number):
+#     # Define the number of classes (days of the week)
+#     num_classes = 6
+#     # Create one-hot encoding
+#     one_hot = tf.one_hot(conference_room_number, num_classes)
+#     return one_hot
+#
+# def get_desk_number(desk):
+#     if desk == "CLUJ_5_beta_18.5":
+#         return 134
+#     if desk == "CLUJ_5_beta_19.5":
+#         return 133
+#     if desk == "CLUJ_5_beta_19.6":
+#         return 132
+#     desk_number = desk.split("_")[3]
+#     desk_number = (int(desk_number.split(".")[0]) - 1) * 4 + (int(desk_number.split(".")[1]) - 1)
+#     return desk_number
+# #
+# #
+# def one_hot_encode_desk(desk_number):
+#     # Define the number of classes (days of the week)
+#     num_classes = 135
+#     # Create one-hot encoding
+#     one_hot = tf.one_hot(desk_number, num_classes)
+#     return one_hot
+#
+#
+# classifiers = [
+#     KNeighborsClassifier(4),
+#     SVC(kernel="linear", C=0.025, random_state=42),
+#     SVC(gamma=2, C=1, random_state=42),
+#     # GaussianProcessClassifier(1.0 * RBF(1.0), random_state=42),
+#     DecisionTreeClassifier(max_depth=5, random_state=42),
+#     RandomForestClassifier(
+#         max_depth=5, n_estimators=10, max_features=1, random_state=42
+#     ),
+#     MLPClassifier(solver='adam', hidden_layer_sizes=(5, 10), random_state=4, alpha=0.5, activation='relu'),
+#     AdaBoostClassifier(algorithm="SAMME", random_state=42),
+#     GaussianNB(),
+#     QuadraticDiscriminantAnalysis(),
+#     XGBClassifier(),
+# ]
 
+workspace_data = loadtxt('hackathon-schema.csv', delimiter=",", skiprows=1, dtype=str)
 
-classifiers = [
-    KNeighborsClassifier(4),
-    SVC(kernel="linear", C=0.025, random_state=42),
-    SVC(gamma=2, C=1, random_state=42),
-    # GaussianProcessClassifier(1.0 * RBF(1.0), random_state=42),
-    DecisionTreeClassifier(max_depth=5, random_state=42),
-    RandomForestClassifier(
-        max_depth=5, n_estimators=10, max_features=1, random_state=42
-    ),
-    MLPClassifier(solver='adam', hidden_layer_sizes=(5, 10), random_state=4, alpha=0.5, activation='relu'),
-    AdaBoostClassifier(algorithm="SAMME", random_state=42),
-    GaussianNB(),
-    QuadraticDiscriminantAnalysis(),
-    XGBClassifier(),
-]
+# desks = np.zeros(135)
+desks = {}
+number_days = 0
 
+for row in workspace_data[:, :]:
+    if row[3] == 'TRUE':
+        deskweek = row[1] + " " + str(get_day_of_week_numeric(row[2]))
+        if deskweek not in desks:
+            desks[deskweek] = 0
+        desks[deskweek] += 1
 
+# good_preds_count=0
+# sorted_desks = sorted(desks)
+#
+# for desk in sorted_desks:
+#     print(desk, desks[desk]/65)
 
-for classifier in classifiers:
-    print(classifier.__class__.__name__)
-    workspace_data = loadtxt('hackathon-schema.csv', delimiter=",", skiprows=1, dtype=str)
-    label_encoder = LabelEncoder()
-    workspace_data[:, 1] = label_encoder.fit_transform(workspace_data[:, 1])
-    workspace_data[:, 3] = label_encoder.fit_transform(workspace_data[:, 3])
-    workspace_data[:, 4] = label_encoder.fit_transform(workspace_data[:, 4])
+# for key, value in desks.items():
+#     print(key, value)
 
-    for i in range(workspace_data.shape[0]):
-        workspace_data[i, 2] = workspace_data[i, 2].split('/')[0]
+correct_guesses = 0
 
-    X = workspace_data[:, 1:3].astype(int)
-    y = workspace_data[:, 3].astype(int)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=7)
+for row in workspace_data[:, :]:
+    deskweek = row[1] + " " + str(get_day_of_week_numeric(row[2]))
+    if desks[deskweek]/13 >= 0.5:
+        if row[3] == "TRUE":
+            correct_guesses += 1
+    else:
+        if row[3] == "FALSE":
+            correct_guesses += 1
 
-    learn = classifier
-    learn.fit(X_train, y_train)
+pickle.dump(desks, open("models/desks-morning.pickle", "ab"))
 
-    pred = learn.predict(X_test)
-    acc = accuracy_score (y_test, pred)
-    print("Acc morning: %.2f%%" % (acc * 100.0))
+print(f"Accuracy morning: {correct_guesses/workspace_data.shape[0]}")
 
-    y = workspace_data[:, 4].astype(int)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=7)
+desks = {}
+for row in workspace_data[:, :]:
+    deskweek = row[1] + " " + str(get_day_of_week_numeric(row[2]))
+    if row[4] == 'TRUE':
+        if deskweek not in desks:
+            desks[deskweek] = 0
+        desks[deskweek] += 1
 
-    learn = classifier
-    learn.fit(X_train, y_train)
+good_preds_count=0
+# print(desks)
+# sorted_desks = sorted(desks)
+#
+# for desk in sorted_desks:
+#     print(desk, desks[desk]/65)
 
-    pred = learn.predict(X_test)
-    acc = accuracy_score (y_test, pred)
-    print("Acc evening: %.2f%%" % (acc * 100.0))
+correct_guesses = 0
 
+for row in workspace_data[:, :]:
+    deskweek = row[1] + " " + str(get_day_of_week_numeric(row[2]))
+    if desks[deskweek]/13 >= 0.5:
+        if row[4] == "TRUE":
+            correct_guesses += 1
+    else:
+        if row[4] == "FALSE":
+            correct_guesses += 1
 
-    workspace_data = loadtxt('meeting-rooms.csv', delimiter=",", skiprows=1, dtype=str)
+pickle.dump(desks, open("models/desks-evening.pickle", "ab"))
 
-    def convert_to_int(value):
-        if value.strip():  # Check if the string is not empty after stripping whitespace
-            return int(value)
-        else:
-            return 0
+print(f"Accuracy evening: {correct_guesses/workspace_data.shape[0]}")
+#
+#
+# print(f"Decent predicitons are{good_preds_count}, of {len(desks)}")
+#
+# days = {}
+#
+# for row in workspace_data[:, :]:
+#     if get_day_of_week_numeric(row[2]) not in days:
+#         days[get_day_of_week_numeric(row[2])] = 0
+#     if row[3] == 'TRUE':
+#         days[get_day_of_week_numeric(row[2])] += 1
+#
+#
+# sorted_days = sorted(days.items(), key=lambda x:x[1])
+#
+#
+# for key, value in days.items():
+#
+#     print(key, value)
+#
+#
+# print("##############################")
+# for day in sorted_days:
+#
+#     print(day[0], days[day[0]])
 
-    label_encoder = LabelEncoder()
-    workspace_data[:, 1] = label_encoder.fit_transform(workspace_data[:, 1])
-    workspace_data[:, 2] = workspace_data[:, 2].astype(int)
-    workspace_data[:, 4] = label_encoder.fit_transform(workspace_data[:, 4])
-    workspace_data[:, 5] = [convert_to_int(val) for val in workspace_data[:, 5]]
-    workspace_data[:, 6] = label_encoder.fit_transform(workspace_data[:, 6])
-    workspace_data[:, 7] = [convert_to_int(val) for val in workspace_data[:, 7]]
-    workspace_data[:, 8] = label_encoder.fit_transform(workspace_data[:, 8])
-    workspace_data[:, 9] = [convert_to_int(val) for val in workspace_data[:, 9]]
-    workspace_data[:, 10] = label_encoder.fit_transform(workspace_data[:, 10])
-    workspace_data[:, 11] = [convert_to_int(val) for val in workspace_data[:, 11]]
+# for classifier in classifiers:
+#     print(classifier.__class__.__name__)
+#     workspace_data = loadtxt('hackathon-schema.csv', delimiter=",", skiprows=1, dtype=str)
+#     label_encoder = LabelEncoder()
+#     desk_data = np.array([[one_hot_encode_desk(get_desk_number(workspace_data[i, 1])),
+#                            one_hot_encode(int(workspace_data[i, 1].split(".")[0].split("_")[3])),
+#                            one_hot_encode(get_day_of_week_numeric(workspace_data[i, 2]))] for i in
+#                           range(workspace_data.shape[0])])
+#
+#     X = np.array([data_entry.flatten() for data_entry in desk_data])
+#     y = label_encoder.fit_transform(workspace_data[:, 3]).astype(int)
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+#
+#     learn = classifier
+#     learn.fit(X_train, y_train)
+#
+#     pred = learn.predict(X_test)
+#     acc = accuracy_score (y_test, pred)
+#     print("Acc morning: %.2f%%" % (acc * 100.0))
+#
+#     y = label_encoder.fit_transform(workspace_data[:, 4]).astype(int)
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=7)
+#
+#     learn = classifier
+#     learn.fit(X_train, y_train)
+#
+#     pred = learn.predict(X_test)
+#     acc = accuracy_score (y_test, pred)
+#     print("Acc evening: %.2f%%" % (acc * 100.0))
+#
+#     workspace_data = loadtxt('meeting-rooms.csv', delimiter=",", skiprows=1, dtype=str)
+#     label_encoder = LabelEncoder()
+#
+#     conference_data = np.array([[one_hot_encode_conference(get_conference_room_number(workspace_data[i, 1])),
+#                                  one_hot_encode_small(get_day_of_week_numeric(workspace_data[i, 3]))] for i in
+#                                 range(workspace_data.shape[0])])
+#     conference_nineToEleven = label_encoder.fit_transform(workspace_data[:, 4])
+#     conference_elevenToOne = label_encoder.fit_transform(workspace_data[:, 6])
+#     conference_oneToThree = label_encoder.fit_transform(workspace_data[:, 8])
+#     conference_threeToFive = label_encoder.fit_transform(workspace_data[:, 10])
+#
+#     X = np.array([data_entry.flatten() for data_entry in conference_data])
+#     y = conference_nineToEleven
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=7)
+#
+#     learn = classifier
+#     learn.fit(X_train, y_train)
+#
+#     pred = learn.predict(X_test)
+#     acc = accuracy_score (y_test, pred)
+#     print("Acc nineToEleven: %.2f%%" % (acc * 100.0))
+#
+#     y = conference_elevenToOne
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=7)
+#
+#     learn = classifier
+#     learn.fit(X_train, y_train)
+#
+#     pred = learn.predict(X_test)
+#     acc = accuracy_score (y_test, pred)
+#     print("Acc elevenToOne: %.2f%%" % (acc * 100.0))
+#
+#     y = conference_oneToThree
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=7)
+#
+#     learn = classifier
+#     learn.fit(X_train, y_train)
+#
+#     pred = learn.predict(X_test)
+#     acc = accuracy_score (y_test, pred)
+#     print("Acc oneToThree: %.2f%%" % (acc * 100.0))
+#
+#     y = conference_threeToFive
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=7)
+#
+#     learn = classifier
+#     learn.fit(X_train, y_train)
+#
+#     pred = learn.predict(X_test)
+#     acc = accuracy_score (y_test, pred)
+#     print("Acc threeToFive: %.2f%%" % (acc * 100.0))
 
-    for i in range(workspace_data.shape[0]):
-        workspace_data[i, 3] = workspace_data[i, 3].split('/')[0]
-
-    X = workspace_data[:, 1:4].astype(int)
-    y = workspace_data[:, 4].astype(int)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=7)
-
-    learn = classifier
-    learn.fit(X_train, y_train)
-
-    pred = learn.predict(X_test)
-    acc = accuracy_score (y_test, pred)
-    print("Acc nineToEleven: %.2f%%" % (acc * 100.0))
-
-    y = workspace_data[:, 6].astype(int)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=7)
-
-    learn = classifier
-    learn.fit(X_train, y_train)
-
-    pred = learn.predict(X_test)
-    acc = accuracy_score (y_test, pred)
-    print("Acc elevenToOne: %.2f%%" % (acc * 100.0))
-
-    y = workspace_data[:, 8].astype(int)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=7)
-
-    learn = classifier
-    learn.fit(X_train, y_train)
-
-    pred = learn.predict(X_test)
-    acc = accuracy_score (y_test, pred)
-    print("Acc oneToThree: %.2f%%" % (acc * 100.0))
-
-    y = workspace_data[:, 10].astype(int)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=7)
-
-    learn = classifier
-    learn.fit(X_train, y_train)
-
-    pred = learn.predict(X_test)
-    acc = accuracy_score (y_test, pred)
-    print("Acc threeToFive: %.2f%%" % (acc * 100.0))
-
-
+#############################################3
 
 # classifiers = [
 #     KNeighborsClassifier(4),
--- backend/ai_component/ai_conference.py
+++ backend/ai_component/ai_conference.py
@@ -1,50 +1,131 @@
+from datetime import datetime
+
 from numpy import loadtxt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import tensorflow as tf
-from numpy import delete
+import numpy as np
 
 def convert_to_int(value):
     return 0
 
+def get_day_of_week_numeric(date_str):
+    # Convert the string to a datetime object
+    date_object = datetime.strptime(date_str, '%d/%m/%Y')
+    # Get the day of the week (0 = Monday, 1 = Tuesday, ..., 6 = Sunday)
+    day_of_week_numeric = date_object.weekday()
+    return day_of_week_numeric
+
+def one_hot_encode(day_of_week_numeric):
+    # Define the number of classes (days of the week)
+    num_classes = 6
+    # Create one-hot encoding
+    one_hot = tf.one_hot(day_of_week_numeric, num_classes)
+    return one_hot
+
+def get_conference_room_number(conference_room):
+    if conference_room == "Quick 8":
+        return 0
+    if conference_room == "Pit-Lane":
+        return 1
+    if conference_room == "Dry-Lane":
+        return 2
+    if conference_room == "Joker Lap":
+        return 3
+    if conference_room == "Pole Position":
+        return 4
+    if conference_room == "Cockpit":
+        return 5
+    return 6
+def one_hot_encode_conference(conference_room_number):
+    # Define the number of classes (days of the week)
+    num_classes = 6
+    # Create one-hot encoding
+    one_hot = tf.one_hot(conference_room_number, num_classes)
+    return one_hot
+
 workspace_data = loadtxt('meeting-rooms.csv', delimiter=",", skiprows=1, dtype=str)
-
 label_encoder = LabelEncoder()
-workspace_data[:, 1] = label_encoder.fit_transform(workspace_data[:, 1])
-workspace_data[:, 2] = workspace_data[:, 2].astype(int)
-workspace_data[:, 4] = label_encoder.fit_transform(workspace_data[:, 4])
-workspace_data[:, 5] = [0 for val in workspace_data[:, 5]]
-workspace_data[:, 6] = label_encoder.fit_transform(workspace_data[:, 6])
-workspace_data[:, 7] = [0 for val in workspace_data[:, 7]]
-workspace_data[:, 8] = label_encoder.fit_transform(workspace_data[:, 8])
-workspace_data[:, 9] = [0 for val in workspace_data[:, 9]]
-workspace_data[:, 10] = label_encoder.fit_transform(workspace_data[:, 10])
-workspace_data[:, 11] = [0 for val in workspace_data[:, 11]]
 
-workspace_data = delete(workspace_data, [5, 7, 9, 11], axis=1)
+conference_data = np.array([[one_hot_encode_conference(get_conference_room_number(workspace_data[i,1])), one_hot_encode(get_day_of_week_numeric(workspace_data[i, 3]))] for i in range(workspace_data.shape[0])])
+conference_nineToEleven = label_encoder.fit_transform(workspace_data[:, 4])
+conference_elevenToOne = label_encoder.fit_transform(workspace_data[:, 6])
+conference_oneToThree = label_encoder.fit_transform(workspace_data[:, 8])
+conference_threeToFive = label_encoder.fit_transform(workspace_data[:, 10])
 
-for i in range(workspace_data.shape[0]):
-    workspace_data[i, 3] = workspace_data[i, 3].split('/')[0]
+X = np.array([data_entry.flatten() for data_entry in conference_data])
+y = conference_nineToEleven
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
-X = workspace_data[:, 1:4].astype(int)
-y = workspace_data[:, 4:12].astype(int)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=7)
+print("Conference data shape:", np.array(X_train).shape)
 
+val = tf.keras.Input(shape=(12,))
 model = tf.keras.Sequential([
-    tf.keras.layers.Dense(27 , activation='relu6'),
-    tf.keras.layers.Dense(4 , activation='gelu'),
-    tf.keras.layers.Dense(12 , activation='relu6'),
-    tf.keras.layers.Dense(4 , activation='sigmoid')
+    val,
+    tf.keras.layers.Dense(24 , activation='relu'),
+    tf.keras.layers.Dense(44 , activation='gelu'),
+    tf.keras.layers.Dense(32 , activation='relu'),
+    tf.keras.layers.Dense(16 , activation='relu'),
+    tf.keras.layers.Dense(8 , activation='gelu'),
+    tf.keras.layers.Dense(1 , activation='sigmoid')
 ])
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-model.fit(X_train, y_train, epochs=50, batch_size=10)
+model.fit(X_train, y_train, epochs=200, batch_size=2)
 
 loss, accuracy = model.evaluate(X_test, y_test)
 print("Test Accuracy:", accuracy)
 
-#predictions = model.predict(X_test)
-#print("Predictions:", predictions)
+predictions = model.predict(X_test)
+# print("Predictions:", predictions)
 
-model.save("C:/Users/Mihai/PycharmProjects/ai/mhp hackathon/backend/ai_component/meeting-rooms.keras")
+formatted_accuracy="{:.2f}".format(accuracy)
+
+model.save(f"C:/Users/Mihai/PycharmProjects/ai/mhp hackathon/backend/ai_component/meeting-rooms-9-11-{formatted_accuracy}.keras")
+
+y = conference_elevenToOne
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+model.fit(X_train, y_train, epochs=200, batch_size=2)
+
+loss, accuracy = model.evaluate(X_test, y_test)
+print("Test Accuracy:", accuracy)
+
+predictions = model.predict(X_test)
+# print("Predictions:", predictions)
+
+formatted_accuracy="{:.2f}".format(accuracy)
+
+model.save(f"C:/Users/Mihai/PycharmProjects/ai/mhp hackathon/backend/ai_component/meeting-rooms-11-13-{formatted_accuracy}.keras")
+
+y = conference_oneToThree
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+model.fit(X_train, y_train, epochs=200, batch_size=2)
+
+loss, accuracy = model.evaluate(X_test, y_test)
+print("Test Accuracy:", accuracy)
+
+predictions = model.predict(X_test)
+# print("Predictions:", predictions)
+
+formatted_accuracy="{:.2f}".format(accuracy)
+
+model.save(f"C:/Users/Mihai/PycharmProjects/ai/mhp hackathon/backend/ai_component/meeting-rooms-13-15-{formatted_accuracy}.keras")
+
+y = conference_threeToFive
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+
+model.fit(X_train, y_train, epochs=200, batch_size=2)
+
+loss, accuracy = model.evaluate(X_test, y_test)
+print("Test Accuracy:", accuracy)
+
+predictions = model.predict(X_test)
+# print("Predictions:", predictions)
+
+formatted_accuracy="{:.2f}".format(accuracy)
+
+model.save(f"C:/Users/Mihai/PycharmProjects/ai/mhp hackathon/backend/ai_component/meeting-rooms-15-17-{formatted_accuracy}.keras")
+
